511d649102a6e87b87ef94d0b5ebc39514974444
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-03-06 14:54:48 +0400

    Finish single article page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import ArticleDetail from "./pages/articleDetail/ArticleDetail";
 import HomePage from "./pages/home/HomePage";
+
+import {Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="font-opensans">
-     <HomePage />
+      <Routes>
+        <Route path='/' index element={ <HomePage />} />
+        <Route path='/blog/:id' element={ <ArticleDetail />} />
+      </Routes>
     </div>
   );
 }
