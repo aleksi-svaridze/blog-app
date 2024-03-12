ac9f2f4067c0d7427bdf32d97a8f0cf3425cc65d
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-03-12 15:57:22 +0400

    Add boolean variables to user actions on comment

--- src/components/comments/CommentsContainer.jsx
+++ src/components/comments/CommentsContainer.jsx
@@ -3,7 +3,7 @@ import CommentsForm from './CommentsForm'
 import { getCommentsData } from '../../data/comments'
 import Comment from './Comment';
 
-export default function CommentsContainer({className}) {
+export default function CommentsContainer({className, loggedInUserId}) {
   const [comments, setComments] = useState([]);
   const mainComments = comments.filter(comment => comment.parent === null);
 
@@ -38,7 +38,7 @@ export default function CommentsContainer({className}) {
 
       <div className='space-y-4 mt-8'>
         {mainComments.map(comment => (
-          <Comment commentData={comment}/>
+          <Comment commentData={comment} loggedInUserId={loggedInUserId}/>
         ))}
       </div>
     </div>
